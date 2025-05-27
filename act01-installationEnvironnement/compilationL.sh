669ac156af626a5b6c29b2866610903caf245a43
PierreViland <132554462+PierreViland@users.noreply.github.com>
2025-05-27 16:36:29 +0200

    Update compilationL.sh

--- act01-installationEnvironnement/compilationL.sh
+++ act01-installationEnvironnement/compilationL.sh
@@ -1,23 +1,5 @@
  #!/bin/bash
 echo "Debut script"
-NOM_TEX=$(basename "$PWD")
-current_dir=$(pwd)
-parent_dir=$(dirname "$current_dir")
-SEQ_DIR=$(basename "$parent_dir")
-echo "Le nom du répertoire juste au-dessus est : $SEQ_DIR"
-TEX_FILE=$(ls *.tex 2>/dev/null | head -n 1)
 
-if [ -z "$TEX_FILE" ]; then
-    echo "Aucun fichier .tex trouvé dans le répertoire courant."
-    exit 1
-fi
-
-if [ "$TEX_FILE" != "${NOM_TEX}.tex" ]; then
-    mv -f "$TEX_FILE" "${NOM_TEX}.tex"
-else
-    echo "Les noms sont identiques, aucune action nécessaire."
-fi
-
-CHEMIN_TEX="$PWD/${NOM_TEX}.tex"
 
 
